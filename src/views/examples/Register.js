5aa7111661954582d2043396ad2368a96d022da9
Oussemamri <oussema9100@gmail.com>
2024-07-31 14:14:46 +0100

    handle errors

--- src/views/examples/Register.js
+++ src/views/examples/Register.js
@@ -155,41 +155,11 @@ const Register = () => {
               <Col lg="5">
                 <Card className="bg-secondary shadow border-0">
                   <CardHeader className="bg-white pb-5">
-                    <div className="text-muted text-center mb-3">
-                      <small>Sign up with</small>
-                    </div>
-                    <div className="text-center">
-                      <Button
-                        className="btn-neutral btn-icon mr-4"
-                        color="default"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        <span className="btn-inner--icon mr-1">
-                          <img
-                            alt="..."
-                            src={require("assets/img/icons/common/github.svg").default}
-                          />
-                        </span>
-                        <span className="btn-inner--text">Github</span>
-                      </Button>
-                      <Button
-                        className="btn-neutral btn-icon ml-1"
-                        color="default"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        <span className="btn-inner--icon mr-1">
-                          <img
-                            alt="..."
-                            src={require("assets/img/icons/common/google.svg").default}
-                          />
-                        </span>
-                        <span className="btn-inner--text">Google</span>
-                      </Button>
-                    </div>
+
                   </CardHeader>
                   <CardBody className="px-lg-5 py-lg-5">
                     <div className="text-center text-muted mb-4">
-                      <small>Or sign up with credentials</small>
+                      <small>sign up with credentials</small>
                     </div>
                     <Form role="form" onSubmit={handleRegister}>
                       <FormGroup>
